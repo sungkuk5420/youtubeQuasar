2113b4ba9e8782821d43d7cc293d9ff6774c3eec
sungkuk5420 <sungkuk5420@gmail.com>
2020-07-26 03:55:29 +0900

    빌드테스트

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -2,32 +2,27 @@
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
     <!-- your content -->
-    <Q-btn
-      v-on:click="goSearch"
-    >
-    　検索画面へ
-    </Q-btn>
+    <Q-btn v-on:click="goSearch">検索画面へ</Q-btn>
   </div>
 </template>
 
 
 <script>
-import { QBtn } from 'quasar'
+import { QBtn } from "quasar";
 
 export default {
   components: {
-    QBtn
+    QBtn,
   },
-  data () {
-    return {}
+  data() {
+    return {};
   },
   methods: {
-    goSearch () {
-      this.$router.push({path: '/search'})
-    }
-  }
-
-}
+    goSearch() {
+      this.$router.push({ path: "/search" });
+    },
+  },
+};
 </script>
 
 <style>
